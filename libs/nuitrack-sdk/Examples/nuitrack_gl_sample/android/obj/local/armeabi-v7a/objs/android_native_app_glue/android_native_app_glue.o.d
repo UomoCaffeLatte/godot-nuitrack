92e20ff8180140de78ccd27ce0bab5ba6a44621f
UomoCaffeLatte <nikhil.reji@live.co.uk>
2020-04-21 10:41:04 +0100

    Folders deleted in restructuring

--- libs/nuitrack-sdk/Examples/nuitrack_gl_sample/android/obj/local/armeabi-v7a/objs/android_native_app_glue/android_native_app_glue.o.d
+++
@@ -1,5 +0,0 @@
-/home/nikhil/Documents/phd-research/godot/godot_nuitrack/libs/nuitrack-sdk/Examples/nuitrack_gl_sample/android/obj/local/armeabi-v7a/objs/android_native_app_glue/android_native_app_glue.o: \
-  /home/nikhil/Documents/phd-research/godot/godot_nuitrack/libs/android-ndk/sources/android/native_app_glue/android_native_app_glue.c \
-  /home/nikhil/Documents/phd-research/godot/godot_nuitrack/libs/android-ndk/sources/android/native_app_glue/android_native_app_glue.h
-
-/home/nikhil/Documents/phd-research/godot/godot_nuitrack/libs/android-ndk/sources/android/native_app_glue/android_native_app_glue.h:
