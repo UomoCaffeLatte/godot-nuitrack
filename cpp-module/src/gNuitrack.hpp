28a9b5d6f1aa15c03dd15c7134017284a8a8cdf7
UomoCaffeLatte <nikhil.reji@live.co.uk>
2020-04-21 12:55:08 +0100

    Refactor gNuitrack.hpp

--- cpp-module/src/gNuitrack.hpp
+++ cpp-module/src/gNuitrack.hpp
@@ -12,29 +12,27 @@ namespace godot {
         GODOT_CLASS(gNuitrack, Node) //Macro for internal setup
 
         private:
-
+            /// gNuitrackCore ///
+            static bool _init_state; // this is only definition
+            static bool _run_state;  // this is only definition
             tdv::nuitrack::SkeletonTracker::Ptr _skeleton_tracker_ptr;
-            tdv::nuitrack::SkeletonData::Ptr _skeleton_data_ptr;
-
             //tdv::nuitrack::UserTracker::Ptr _user_tracker_ptr;
             //tdv::nuitrack::ColorSensor::Ptr _color_sensor_ptr;
 
+            /// gNuitrackSkeleton ///
             int _num_skeletons;
-            static bool _init_state; // this is only definition
-            static bool _run_state;  // this is only definition
-
+            tdv::nuitrack::SkeletonData::Ptr _skeleton_data_ptr;
             void on_update_skeleton(tdv::nuitrack::SkeletonData::Ptr skeleton_data); //obtain skeleton joint data here
 
         public:
-
+            /// gNuitrackGetter ///
             int get_num_skeletons() const;
             bool get_init_state() const;
 
+            /// gNuitrackCore ///
             static void _register_methods();
             void _init(); //initialiser called by Godot
             ~gNuitrack();
-
-            
             bool init();
             void update();
             void create(bool skeleton_tracker, bool user_tracker, bool color_sensor);
